f9a6c33122056e5f868cf3c451b8df268fe3fdde
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-08-16 12:24:54 +0200

    feat: aggiunto carrello

--- src/componets/ListProduct.jsx
+++ src/componets/ListProduct.jsx
@@ -1,21 +1,45 @@
 import { useState } from "react"
 import products from "../../data/data"
 
-export default function ListPruduct(){
-    const [addProducts, setAddProduct] = useState([])
+export default function ListPruduct() {
+    const [addProducts, setAddProducts] = useState([])
+
+    function addToCart(product) {
+        const isProductInCart = addProducts.some(p => p.name === product.name)
+        if (isProductInCart) {
+            return
+        }
+        setAddProducts(curr => ([...curr, {
+            ...product,
+            quantity: 1
+        }]))
+    }
 
 
-
-
-    return(
+    return (
         <>
-        <ul>
-            {products.map((product, i) =>{
-                return(
-                    <li key={i}>{product.name}:{product.price.toFixed(2)} €</li>
-                )
-            })}
-        </ul>
+            <ul>
+                {products.map((product, i) => {
+                    return (
+                        <li key={i}>
+                            <p>{product.name}:{product.price.toFixed(2)} €</p>
+                            <button onClick={() => addToCart(product)}>Aggingi al carrello</button>
+                        </li>
+                    )
+                })}
+            </ul>
+            {addProducts.length > 0 && (
+                <ul>
+                    <h2>Carrello</h2>
+                    {addProducts.map((product, i) => {
+                       return(
+                        <li key={i}>
+                            <p>{product.quantity}:{product.name}({product.price.toFixed(2)} €)</p>
+                        </li>
+                       ) 
+                    })}
+                </ul>
+            )}
 
         </>
     )
